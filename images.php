d98fec826845b7ef2235dbfd1c142db09388aa57
BrankoSabo <ssabobranko@gmail.com>
2019-12-17 18:51:55 +0100

    watermark

---
+++ images.php
@@ -0,0 +1,10 @@
+<?php
+    $x = "";
+    function images(){
+        echo "<div class=\"image-container\">";
+        // foreach(x as y){
+        //     echo "<div class=\"image\"><img src=\"xxxxxx\" alt=\"YYYYY\"></div>";
+        // }
+    }
+    echo "</div>";
+    ?>
---
+++ watermark.php
@@ -0,0 +1,21 @@
+<?php
+$source = "images";
+$destination = "destination";
+$watermark = imagecreatefrompng("watermark.png");
+
+$margin_right = 10;
+$margine_bottom  = 10;
+
+$sx = imagesx($watermark);
+$sy = imagesy($watermark);
+
+$images = array_diff(scandir($source), array('..','.'));
+
+foreach ( $images as $image){
+    $img = imagecreatefromjpeg($source.'/'.$image);
+
+    imagecopy($img, $watermark, imagesx($img)-$sx-$margin_right, imagesy($img)-$sy-$margine_bottom,0,0,$sx,$sy);
+    $i = imagejpeg($img, $destination.'/'.$image, 100);
+    imagedestroy($img);
+}
+?>
