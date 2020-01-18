7bcbb917e165393c4882801e1ef5ac2cd2bee253
BrankoSabo <ssabobranko@gmail.com>
2020-01-18 23:16:52 +0100

    mobile
    mobile temp/testing setup

---
+++ mobile/index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mobile</title>
+</head>
+<body>
+    <!-- login -->
+
+
+
+    <!-- name -->
+    <!-- earning -->
+    <!-- total unique downloads -->
+    <hr>
+    <!-- img  -->
+    <!-- title  -->
+    <!-- total downloads -->
+    <!-- unique downloads -->
+    <!-- earning  -->
+<!-- repeat  -->
+</body>
+</html>
---
+++ mobile/login.inc.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include("db_config.php");
+//include("../verify.php");
+
+$id=0;
+$username = mysqli_real_escape_string($conn, $_POST["username"]);
+$password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+$sql = "SELECT * FROM user WHERE username = '%{$username}%'";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) > 0 or !password_verify($password,$result['password']))
+{
+    while($row = $result->fetch_array())//$row = mysqli_fetch_array($result, MYSQLI_BOTH))
+    {
+        $id = $row["id_user"];
+        $power = $row["power"];
+        $verified = $row["verified"];
+    }
+    if($verified==0){
+        header("Location: ../index.php?error=666");
+    }
+    $_SESSION["id_user"] = $id;
+    $_SESSION["username"] = $username;
+    $_SESSION["admin"] = $power;
+    header("Location: ../index.php");
+    exit();
+}
+else {
+    header("Location: ../index.php?error=2");
+    exit();
+}
