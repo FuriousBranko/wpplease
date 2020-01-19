c53dc7ad0fba0b1f8ca9a52022225e65ded1c5b3
BrankoSabo <ssabobranko@gmail.com>
2020-01-19 16:53:03 +0100

    SQL v2
    updated old sql with new data and columns

--- legacy/wallpaperme.sql
+++ legacy/wallpaperme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2020 at 04:51 PM
+-- Generation Time: Dec 18, 2019 at 12:33 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -47,18 +47,15 @@ CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(30) NOT NULL,
-  `power` int(11) NOT NULL,
-  `verified` tinyint(1) NOT NULL DEFAULT 0,
-  `vkey` text NOT NULL
+  `power` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `username`, `email`, `password`, `power`, `verified`, `vkey`) VALUES
-(1, 'user', 'user@user.user', '$2y$10$RPvBhcOauR4Vdp8.I9z6q.a', 0, 0, ''),
-(2, 'user2', 'user2@user.user', '$2y$10$VobQ8yzCyNGYwia1mW8X.e4', 0, 0, '');
+INSERT INTO `user` (`id_user`, `username`, `email`, `password`, `power`) VALUES
+(1, 'user', 'user@user.user', '$2y$10$RPvBhcOauR4Vdp8.I9z6q.a', 0);
 
 -- --------------------------------------------------------
 
@@ -71,23 +68,21 @@ CREATE TABLE `wallpaper` (
   `id_user` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `downloads` varchar(255) NOT NULL,
-  `unique_downloads` int(11) NOT NULL DEFAULT 0,
   `mobile` text NOT NULL,
   `desktop` text NOT NULL,
-  `tags` varchar(20) NOT NULL,
-  `verified` tinyint(1) NOT NULL DEFAULT 0
+  `tags` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `wallpaper`
 --
 
-INSERT INTO `wallpaper` (`id_wallpaper`, `id_user`, `title`, `downloads`, `unique_downloads`, `mobile`, `desktop`, `tags`, `verified`) VALUES
-(1, 1, 'Dog 1', '', 0, '', 'doggo1.jpg', 'dog', 0),
-(2, 1, 'Dog 2', '', 0, '', 'doggo2.jpg', 'dog', 0),
-(3, 1, 'Dog 3', '', 0, '', 'doggo3.jpg', 'dog', 0),
-(4, 1, 'Bird 1', '', 0, '', 'eagle.jpg', 'bird', 0),
-(5, 1, 'Bird 2', '', 0, '', 'og_bird.jpg', 'bird', 0);
+INSERT INTO `wallpaper` (`id_wallpaper`, `id_user`, `title`, `downloads`, `mobile`, `desktop`, `tags`) VALUES
+(1, 1, 'Dog 1', '', '', 'doggo1.jpg', 'dog'),
+(2, 1, 'Dog 2', '', '', 'doggo2.jpg', 'dog'),
+(3, 1, 'Dog 3', '', '', 'doggo3.jpg', 'dog'),
+(4, 1, 'Bird 1', '', '', 'eagle.jpg', 'bird'),
+(5, 1, 'Bird 2', '', '', 'og_bird.jpg', 'bird');
 
 --
 -- Indexes for dumped tables
@@ -127,7 +122,7 @@ ALTER TABLE `subscription`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `wallpaper`
