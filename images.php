d9f9bc3e79ac55a2addea732bc69be9b522bc928
BrankoSabo <ssabobranko@gmail.com>
2020-01-19 20:53:20 +0100

    search fix

--- images.php
+++ images.php
@@ -2,11 +2,11 @@
 //include "includes\db_config.php";
 
     if(isset($_GET['search'])){
-
+        // $search = $_GET['search'];
         $search = trim(strtolower($_GET['search']));
-
-        $result= mysqli_query($conn, "SELECT * FROM wallpaper WHERE tags = '%{$search}%'");
-    
+        //var_dump($search);
+        $result= mysqli_query($conn, "SELECT * FROM `wallpaper` WHERE `tags` = '$search'");
+        //var_dump($result);
         if(mysqli_num_rows($result)>0){
             
             echo "<div class=\"container\"><div class=\"row\">";
